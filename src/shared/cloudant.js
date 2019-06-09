39246ff96406f9b0b84db9cf8b0e31de1c002c22
Danial Manavi <dmanavi@live.com>
2019-06-09 16:06:27 +0430

    Add db.use.
    Changes to be committed:
    	modified:   src/shared/cloudant.js

--- src/shared/cloudant.js
+++ src/shared/cloudant.js
@@ -5,32 +5,37 @@
 'use strict';
 
 const Cloudant = require('@cloudant/cloudant');
-const env = require("env-var").get;
+const env = require('env-var').get;
 
 module.exports.connect = async () => {
 
     return new Promise((resolve, reject) => {
 
-        console.debug("Connecting to the cloudant server...");
+        console.debug('Connecting to the cloudant server...');
 
         Cloudant(
             {
-                url: env("CITYPLUS_DB_CLOUDANT_URL").required().asUrlString(),
-                account: env("CITYPLUS_DB_CLOUDANT_USERNAME").required().asString(),
-                password: env("CITYPLUS_DB_CLOUDANT_PASSWORD").required().asString()
+                url: env('CITYPLUS_DB_CLOUDANT_URL').required().asUrlString(),
+                account: env('CITYPLUS_DB_CLOUDANT_USERNAME').required().asString(),
+                password: env('CITYPLUS_DB_CLOUDANT_PASSWORD').required().asString()
             },
             (err, client) => {
 
                 if (err) {
 
-                    console.error("Failed to connect to the cloudant server.", err);
+                    console.error('Failed to connect to the cloudant server.', err);
 
                     return reject(err);
                 }
 
-                console.debug("Connected to the cloudant server.");
+                console.debug('Connected to the cloudant server.');
 
-                return resolve(client);
+                const dbName = env('CITYPLUS_DB_CLOUDANT_DB').required().asString();
+                console.debug(`Using database ${dbName}...`);
+
+                const db = client.db.use(dbName);
+
+                return resolve({ client, db });
             }
         );
     });
